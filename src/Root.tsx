00f42e886ffa89f71d9c0b6b9ca90b12d0c97033
dokechi <mekazawakun2525@gmail.com>
2026-07-26 16:51:56 +0900

    Add 3D retirement insurance explainer composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,10 @@
 import {Composition} from 'remotion';
 import {MainVideo} from './MainVideo';
 import {insuranceSample} from './data/insurance-sample';
+import {RetirementInsuranceComic3D} from './RetirementInsuranceComic3D';
+import {retirementInsuranceData} from './data/retirement-insurance';
 
-export const RemotionRoot = () => <Composition id="InsuranceShort" component={MainVideo} durationInFrames={900} fps={30} width={1080} height={1920} defaultProps={{data: insuranceSample}} />;
+export const RemotionRoot = () => <>
+  <Composition id="InsuranceShort" component={MainVideo} durationInFrames={900} fps={30} width={1080} height={1920} defaultProps={{data: insuranceSample}} />
+  <Composition id="TaishokuShakaihokenKokuhou3D" component={RetirementInsuranceComic3D} durationInFrames={1200} fps={30} width={1080} height={1920} defaultProps={{data: retirementInsuranceData}} />
+</>;
